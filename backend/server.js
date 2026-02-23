8d9f08911e6456f304dca1ed50f92213794ed88c
Igor Manusha <igormanushaviktor@gmail.com>
2026-02-23 19:52:50 +0200

    add api privat

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,65 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const axios = require("axios");
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./base.db");
+
+const PORT = process.env.PORT || 3000;
+
+app.post("/api/data", (req, res) => {
+  const { istochnik, summa } = req.body;
+  db.run(
+    "INSERT INTO dohod (istochnik, summa) VALUES (?, ?)",
+    [istochnik, summa],
+    function (err) {
+      if (err) {
+        return res.status(500).json({ error: err.message });
+      }
+      res.json({ id: this.lastID });
+    },
+  );
+});
 
 // Sample route
+
+app.get("/api/kursprivat", (req, res) => {
+  const getDataKurs = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5",
+      );
+      res.json(response.data);
+    } catch (error) {
+      console.error("Error fetching currency data:", error);
+      res.status(500).json({ error: "Failed to fetch currency data" });
+    }
+  };
+  getDataKurs();
+});
+
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from the server!" });
+  db.all("SELECT * FROM dohod", [], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.delete("/api/del/:id", (req, res) => {
+  const { id } = req.params;
+  db.run("DELETE FROM dohod WHERE id = ?", [id], function (err) {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ message: "Record deleted", changes: this.changes });
+  });
 });
 
 app.listen(PORT, () => {
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import Cashflow from "./components/Cashflow";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Cashflow />
+      </header>
+    </div>
+  );
+}
+
+export default App;
